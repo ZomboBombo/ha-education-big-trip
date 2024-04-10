5138c6062c09bf1717c45553ba766c644ee62f18
Sergey Novikov <foxinazombobombo@mail.ru>
2024-04-10 11:30:40 +0700

    core: upd webpack.config accordingly with .editorconfig rules

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,45 @@ const HtmlPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-    },
-    devtool: 'source-map',
-    plugins: [
-        new HtmlPlugin({
-            template: 'public/index.html',
-        }),
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
 
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: 'public',
-                    globOptions: {
-                        ignore: ['**/index.html'],
-                    },
-                },
-            ],
-        }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              },
-            },
-          },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
-          },
-        ],
-    },
+  },
 };
